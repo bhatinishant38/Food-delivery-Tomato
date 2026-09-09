a82b9ed78ae0395a1e49a6d0fa9ab8bee8f9c619
Nishant <bhatinishant38@gmail.com>
2026-09-09 21:22:50 +0530

    fix: Uncomment database and server connection logs for better debugging

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,5 @@ app.get("/", (req, res, next) => {
 });
 
 app.listen(port, () => {
-  // console.log(`server running on http://localhost:${port}`)
+  console.log(`server running on http://localhost:${port}`)
 });
